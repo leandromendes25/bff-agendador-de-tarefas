061eb5c689d60458af44992eb98e03edc1a58e3b
leandromendes25 <leandromiendes@hotmail.com>
2026-01-13 14:19:39 -0300

    adicionado esqueleto projeto

---
+++ src/test/java/com/leandromendes25/BFF/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandromendes25.BFF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
